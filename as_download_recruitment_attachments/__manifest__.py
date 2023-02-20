62a992e7c941dff193d4ef903c9499468d80d670
aemal-shirzai <aemalshirzai2016@gmail.com>
2023-02-20 01:10:13 -0500

    [ADD] Add module structure.

---
+++ as_download_recruitment_attachments/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Download Batch Recruitment Attachments",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "Aemal Shirzai",
+    'website': "https://aemal-shirzai.github.io/portfolio/",
+    'category': 'Human Resources/Recruitment',
+    'version': '0.1',
+    'depends': ['base', 'hr_recruitment'],
+    'data': [],
+}
